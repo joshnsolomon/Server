ed54844457fb20a5454acfb4ac0148eb511bcca6
joshnsolomon <joshnsolomon@gmail.com>
2024-05-18 16:43:57 -0700

    reverse image colors on mouse hover

--- page/script.js
+++ page/script.js
@@ -1,1 +1,15 @@
 console.log("Hellow, WORLD!!");
+
+//reverse the color of the image when you hover over it.
+const image = document.getElementById('flowers');
+
+const org = 'flowers3.png';
+const rev = 'rflowers3.png';
+
+image.addEventListener('mouseover', () => {
+    image.src = rev;
+});
+
+image.addEventListener('mouseout', () => {
+    image.src = org;
+});
